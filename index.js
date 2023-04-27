80a48f6dd2e7b7e0e469013238d14bc2d013fb74
Atharv Vyas <95745167+Mr-okey@users.noreply.github.com>
2023-04-27 21:47:07 +0530

    minor fixesq

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const asdfg = document.getElementById("asdfg");
 const zxcvb = document.getElementById("zxcvb");
 const bottomKeys = document.getElementById("bottomKeys");
 const font_size = document.getElementById("font_size")
-font_size.value = "20"
+font_size.value = "22"
 
 const keyArray = ["\`", '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
     'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
@@ -54,36 +54,48 @@ dispKeyBoard()
 // highlight key onkeyDown event
 let prevKey = document.getElementById("Space")
 let currKey
+
 document.getElementById("textarea").onkeydown = (key) => {
     prevKey.style.background = "white"
     key = (key || window.event)
 
     let indx = keyArray.indexOf(key.key)
-    if (indx == -1) {
+    if (indx === -1) {
         indx = shiftKeys.indexOf(key.key)
-        if (indx == -1 ) {
+        if (indx === -1) {
+            indx = 55
             return
         }
     }
-    if (key.key === " ") {
-        indx = shiftKeys.indexOf("Space")
+    console.log(shiftKeys.indexOf("Space"))
+    if (key.key === "") {
+        indx = 55
     }
 
     currKey = document.getElementById(keyArray[indx])
     currKey.style.background = "#9caad9"
     prevKey = currKey
-
-    document.getElementById("textarea").style.fontSize = document.getElementById("font_size").value + "px"
-    document.getElementById("textarea").style.color = document.getElementById("font_color").value
 };
 
-document.getElementById("clear").addEventListener("click", ()=>{
+// event to reflect changes instantly in the text area
+document.getElementById("font_size").addEventListener("input", (e) => {
+    document.getElementById("textarea").style.fontSize = e.target.value + "px"
+})
+
+document.getElementById("font_color").addEventListener("input", (e) => {
+    document.getElementById("textarea").style.color = e.target.value
+})
+
+//  clear the text area
+document.getElementById("clear").addEventListener("click", () => {
     document.getElementById("textarea").value = ""
 })
-document.getElementById("copyToClipboard").addEventListener("click", ()=>{
+
+// copy to clip board 
+document.getElementById("copyToClipboard").addEventListener("click", () => {
     let text = document.getElementById("textarea").value
     document.getElementById("copyToClipboard").innerHTML = "copied"
-    setTimeout(()=>{
+    setTimeout(() => {
         document.getElementById("copyToClipboard").innerHTML = "copy to clipboard"
     }, 1000)
     navigator.clipboard.writeText(text);
